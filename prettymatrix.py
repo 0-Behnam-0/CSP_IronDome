5f182c55abdeb9b3dfe794fd302b13639f0ad329
0-Behnam-0 <Mahdi.mohamadiha.znu@gmail.com>
2023-04-17 02:48:00 +0430

    New package for matrix UI

---
+++ prettymatrix.py
@@ -0,0 +1,47 @@
+def pretty_matrix(matrix):
+
+    row_break = len(matrix[0])
+
+    wise_list = []
+    for row in matrix:
+        for wise in row:
+            wise_list.append(wise)
+
+    max_col_length_list = []
+    row_index = 0
+    while row_index < row_break:
+        col_index = row_index
+        col_list = []
+        while col_index < len(wise_list):
+            col_list.append(str(wise_list[col_index]))
+            col_index += row_break
+        max_col_length_list.append(len(max(col_list, key=len)))
+        row_index += 1
+
+    row_length = 0
+    for ele in max_col_length_list:
+        row_length = row_length + ele
+    row_length = row_length + 2*(1+len(max_col_length_list)) - 4
+
+    print("+--" + row_length*" " + "--+")
+
+    for row in matrix:
+        index = 0
+        print("¦", end="")
+        for wise in row:
+            padding = max_col_length_list[index] - len(str(wise))
+            print("  " + str(wise), end="")
+            print(padding*" ", end="")
+            index += 1
+        print("  ¦")
+
+    print("+--" + row_length*" " + "--+")
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [1, 2, 64646460, 0],
+    [46, -3, -5, 3]
+]
+
+pretty_matrix(matrix)
