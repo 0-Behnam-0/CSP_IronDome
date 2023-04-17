8b0738de214ee7a9d20eed557c517ff12e65891b
0-Behnam-0 <Mahdi.mohamadiha.znu@gmail.com>
2023-04-17 18:29:24 +0430

    first matrix draw

--- core.py
+++ core.py
@@ -1,30 +1,15 @@
 from CSP_assistant import *
 
+targets = int(input("Enter number of Targets: "))
+sensors = int(input("Enter number of Sensors: "))
 
 matrix = []
+row_list = []
+for i in range(sensors):
+    for j in range(targets):
+        row_list.append("0")
+    matrix.append(row_list.copy())
+    row_list.clear()
 
-t_cnt = 0
-target = True
-target_list = []
-while target:
-    t_cnt += 1
-    target = input("["+ str(t_cnt) +"] Enter target(leave blank to exit): ")
-    target_list.append(target)
-target_list.pop(-1)
-matrix.append(target_list)
-
-s_cnt = 0
-sensor = True
-sensor_list = []
-while sensor:
-    s_cnt += 1
-    sensor = input("["+ str(s_cnt) +"] Enter sensor(leave blank to exit): ")
-    sensor_list.append(sensor)
-sensor_list.pop(-1)
-
-t_cnt = t_cnt - 1
-sensor_list = [sensor_list[i:i + t_cnt] for i in range(0, len(sensor_list), t_cnt)]
-for list in sensor_list:
-    matrix.append(list)
 
 pretty_matrix(matrix)
